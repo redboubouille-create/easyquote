5a6ce262f9f0ac89d9841b54cf63936842a9f1d4
redboubouille-create <302217383+redboubouille-create@users.noreply.github.com>
2026-07-11 02:39:16 +0200

    Premier tableau de bord Hestia

--- js/storage.js
+++ js/storage.js
@@ -1,47 +1,158 @@
-const HestiaData = {
+document.addEventListener("DOMContentLoaded", () => {
 
-    entreprise: {
-        nom: "",
-        prenom: "",
-        telephone: "",
-        email: ""
-    },
+    demarrerHestia();
 
-    clients: [],
-
-    devis: [],
-
-    planning: [],
-
-    factures: [],
+});
 
 
-    sauvegarder(){
+function demarrerHestia(){
 
-        localStorage.setItem(
-            "hestiaData",
-            JSON.stringify(this)
-        );
+    if(HestiaData.entreprise.nom === ""){
 
-    },
+        afficherCreationEntreprise();
 
+    } else {
 
-    charger(){
-
-        const data = localStorage.getItem("hestiaData");
-
-        if(data){
-
-            Object.assign(
-                this,
-                JSON.parse(data)
-            );
-
-        }
+        afficherDashboard();
 
     }
 
-};
+}
 
 
-HestiaData.charger();
+
+function afficherCreationEntreprise(){
+
+const container = document.querySelector(".container");
+
+
+container.innerHTML = `
+
+<h1>🏛️ Hestia</h1>
+
+<p>
+Créons votre espace professionnel
+</p>
+
+
+<input id="nomEntreprise"
+placeholder="Nom de l'entreprise">
+
+
+<input id="prenom"
+placeholder="Votre prénom">
+
+
+<input id="telephone"
+placeholder="Téléphone">
+
+
+<button onclick="creerEntreprise()">
+
+Créer mon espace
+
+</button>
+
+`;
+
+}
+
+
+
+function creerEntreprise(){
+
+HestiaData.entreprise.nom =
+document.getElementById("nomEntreprise").value;
+
+
+HestiaData.entreprise.prenom =
+document.getElementById("prenom").value;
+
+
+HestiaData.entreprise.telephone =
+document.getElementById("telephone").value;
+
+
+HestiaData.sauvegarder();
+
+
+afficherDashboard();
+
+}
+
+
+
+function afficherDashboard(){
+
+const container = document.querySelector(".container");
+
+
+container.innerHTML = `
+
+<h1>
+🏛️ Hestia
+</h1>
+
+
+<p>
+Bonjour ${HestiaData.entreprise.prenom} 👋
+</p>
+
+
+<div>
+
+👥 Clients :
+${HestiaData.clients.length}
+
+</div>
+
+
+<br>
+
+
+<div>
+
+🧾 Devis :
+${HestiaData.devis.length}
+
+</div>
+
+
+<br>
+
+
+<div>
+
+📅 Interventions :
+${HestiaData.planning.length}
+
+</div>
+
+
+<br>
+
+
+<div>
+
+💰 Factures :
+${HestiaData.factures.length}
+
+</div>
+
+
+<br><br>
+
+
+<button>
+➕ Nouveau devis
+</button>
+
+
+<button>
+🤖 Hermès
+</button>
+
+
+`;
+
+}
